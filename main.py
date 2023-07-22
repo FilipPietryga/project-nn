ec2356fa0bcd4770c53e694838ee778f13f53703
FilipPietryga <filippatrykpietryga@gmail.com>
2023-07-22 15:51:45 +0200

    minor fix

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ with mp_pose.Pose(min_detection_confidence=5, min_tracking_confidence=0.5) as po
     print(result)
     
     image.flags.writeable = True
-    image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     
     mp_drawing.draw_landmarks(image, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
     
-    cv2.imshow("", image)    
+    cv2.imshow("mediapipe feed", image)    
     
     if cv2.waitKey(10) & 0xFF == ord("q"):
       break
