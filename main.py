0b579d836dd4cbdbf3f37547761bbda1019c5feb
FilipPietryga <filippatrykpietryga@gmail.com>
2023-07-22 15:50:34 +0200

    minor fixes

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ with mp_pose.Pose(min_detection_confidence=5, min_tracking_confidence=0.5) as po
   while cap.isOpened():
     ret, frame = cap.read()
     
-    image = cv2.cvtColor(frame, cv2.COLOR_BAYER_BGR2RGB)
-    image.flags.writable = False
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
 
     result = pose.process(image)
     
-    image.flags.writable = True
-    image = cv2.cvtColor(frame, cv2.COLOR_BAYER_RGB2BGR)
+    print(result)
     
-    cv2.imshow("", frame)    
+    image.flags.writeable = True
+    image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    
+    mp_drawing.draw_landmarks(image, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+    
+    cv2.imshow("", image)    
     
     if cv2.waitKey(10) & 0xFF == ord("q"):
       break
