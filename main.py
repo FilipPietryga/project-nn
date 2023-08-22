b8f0f910dbe63d2a8bf68709b7f3e414a4e89227
FilipPietryga <filippatrykpietryga@gmail.com>
2023-08-22 22:49:27 +0200

    introduced the interface

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 mp_drawing  = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QPushButton
+
 jacks = 0
 stage = "top"
 
@@ -19,75 +21,245 @@ def calculate_angle(a,b,c):
         angle = 360-angle
         
     return angle 
+  
+# class Okno(QWidget):
+#   def __init__(self, parent=None):
+#       super().__init__(parent)
 
-cap = cv2.VideoCapture(0)
-## Setup mediapipe instance
-with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-    while cap.isOpened():
-        ret, frame = cap.read()
-        
-        # Recolor image to RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
-      
-        # Make detection
-        results = pose.process(image)
+#       self.interfejs()
+
+#   def interfejs(self):
     
-        # Recolor back to BGR
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+#       etykieta1 = QLabel("Liczba pajacykow:" + str(jacks), self)
+#       guzik = QPushButton("resetuj liczbe pajacykow")
+
+#       # przypisanie widgetów do układu tabelarycznego
+#       ukladT = QGridLayout()
+#       ukladT.addWidget(etykieta1, 0, 0)
+#       ukladT.addWidget(guzik, 1, 0)
+
+#       self.resize(300, 100)
+#       self.setWindowTitle("Aplikacja do zliczania pajacykow z uzyciem AI")
+#       self.show()
+
+# cap = cv2.VideoCapture(0)
+# ## Setup mediapipe instance
+# with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+#     while cap.isOpened():
+#         ret, frame = cap.read()
         
-        # Extract landmarks
-        try:
-            landmarks = results.pose_landmarks.landmark
+#         # Recolor image to RGB
+#         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#         image.flags.writeable = False
+      
+#         # Make detection
+#         results = pose.process(image)
+    
+#         # Recolor back to BGR
+#         image.flags.writeable = True
+#         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+#         # Extract landmarks
+#         try:
+#             landmarks = results.pose_landmarks.landmark
             
-            # Get coordinates
-            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
-            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+#             # Get coordinates
+#             shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+#             elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+#             wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
             
-            hips = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
-            knees = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
-            ankles = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+#             hips = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+#             knees = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+#             ankles = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
             
             
-            # Calculate angle
-            angle_top = calculate_angle(shoulder, elbow, wrist)
-            angle_bottom = calculate_angle(hips, knees, ankles)
+#             # Calculate angle
+#             angle_top = calculate_angle(shoulder, elbow, wrist)
+#             angle_bottom = calculate_angle(hips, knees, ankles)
 
 
-            # Visualize angle
-            cv2.putText(image, str(angle_top),
-                           tuple(np.multiply(elbow, [640, 480]).astype(int)),
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                                )
+#             # Visualize angle
+#             cv2.putText(image, str(angle_top),
+#                            tuple(np.multiply(elbow, [640, 480]).astype(int)),
+#                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+#                                 )
 
 
-            #print(angle_top)
+#             #print(angle_top)
 
-            # Curl counter logic
-            if angle_top < 5 and angle_bottom < 5:
-              stage = "started"
-            elif angle_top > 25 and angle_bottom > 25:
-              if stage =="started":
-                stage="finished"
-                jacks += 1
-                print(jacks)
+#             # Curl counter logic
+#             if angle_top < 5 and angle_bottom < 5:
+#               stage = "started"
+#             elif angle_top > 25 and angle_bottom > 25:
+#               if stage =="started":
+#                 stage="finished"
+#                 jacks += 1
+#                 print(jacks)
                        
-        except:
-            pass
+#         except:
+#             pass
         
         
-        # Render detections
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
-                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
-                                 )               
+#         # Render detections
+#         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+#                                 mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+#                                 mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+#                                  )               
         
-        cv2.imshow('Mediapipe Feed', image)
+#         cv2.imshow('Mediapipe Feed', image)
 
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
+#         if cv2.waitKey(10) & 0xFF == ord('q'):
+#             break
 
-    cap.release()
-    cv2.destroyAllWindows()
+#     cap.release()
+#     cv2.destroyAllWindows()
+    
+
+
+
+# if __name__ == '__main__':
+#     import sys
+
+#     app = QApplication(sys.argv)
+#     okno = Okno()
+#     sys.exit(app.exec_())
+    
+jacks = 0
+    
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout, QPushButton, QGridLayout
+from PyQt5.QtGui import QPixmap
+import sys
+import cv2
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread
+import numpy as np
+
+class VideoThread(QThread):
+    change_pixmap_signal = pyqtSignal(np.ndarray)
+
+    def run(self):
+        cap = cv2.VideoCapture(0)
+        with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+          while cap.isOpened():
+            ret, frame = cap.read()
+            
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+          
+            # Make detection
+            results = pose.process(image)
+        
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            
+            # Extract landmarks
+            try:
+                landmarks = results.pose_landmarks.landmark
+                
+                # Get coordinates
+                shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+                elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+                
+                hips = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+                knees = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+                ankles = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+                
+                
+                # Calculate angle
+                angle_top = calculate_angle(shoulder, elbow, wrist)
+                angle_bottom = calculate_angle(hips, knees, ankles)
+
+
+                # Visualize angle
+                # cv2.putText(image, str(angle_top),
+                #                 tuple(np.multiply(elbow, [640, 480]).astype(int)),
+                #                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                #                     )
+
+
+                #print(angle_top)
+
+                # Curl counter logic
+                if angle_top < 5 and angle_bottom < 5:
+                  stage = "started"
+                elif angle_top > 25 and angle_bottom > 25:
+                  if stage =="started":
+                    stage="finished"
+                    jacks += 1
+                    print(jacks)
+                            
+            except:
+                pass
+            
+            
+            # Render detections
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                      )               
+            
+            #cv2.imshow('Mediapipe Feed', image)
+            self.change_pixmap_signal.emit(image)
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Qt live label demo")
+        self.disply_width = 640
+        self.display_height = 480
+        # create the label that holds the image
+        self.image_label = QLabel(self)
+        self.image_label.resize(self.disply_width, self.display_height)
+        # create a text label
+        
+        etykieta1 = QLabel("Liczba pajacykow:" + str(jacks), self)
+        guzik = QPushButton("resetuj liczbe pajacykow")
+
+        self.resize(300, 100)
+        self.setWindowTitle("Aplikacja do zliczania pajacykow z uzyciem AI")
+        self.show()
+
+        # create a vertical box layout and add the two labels
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.image_label)
+        vbox.addWidget(etykieta1)
+        vbox.addWidget(guzik)
+        # set the vbox layout as the widgets layout
+        self.setLayout(vbox)
+
+        # create the video capture thread
+        self.thread = VideoThread()
+        # connect its signal to the update_image slot
+        self.thread.change_pixmap_signal.connect(self.update_image)
+        # start the thread
+        self.thread.start()
+
+    @pyqtSlot(np.ndarray)
+    def update_image(self, cv_img):
+        """Updates the image_label with a new opencv image"""
+        qt_img = self.convert_cv_qt(cv_img)
+        self.image_label.setPixmap(qt_img)
+    
+    def convert_cv_qt(self, cv_img):
+        """Convert from an opencv image to QPixmap"""
+        rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+        h, w, ch = rgb_image.shape
+        bytes_per_line = ch * w
+        convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
+        p = convert_to_Qt_format.scaled(self.disply_width, self.display_height, Qt.KeepAspectRatio)
+        return QPixmap.fromImage(p)
+    
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    a = App()
+    a.show()
+    sys.exit(app.exec_())
